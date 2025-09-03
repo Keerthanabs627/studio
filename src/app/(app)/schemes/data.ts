a714d8bf3e972362eb9f3b70fc5666dfd9a5eac2
Keerthana Bs <keerthanabs627@gmail.com>
2025-09-03 13:11:43 +0000

    The sections are not opening properly

--- src/app/(app)/schemes/data.ts
+++ src/app/(app)/schemes/data.ts
@@ -1,3 +1,4 @@
+
 // @ts-nocheck
 import type { Scheme } from './actions';
 
--- src/app/(app/schemes/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import { getSchemes } from './actions';
-import { SchemesClient } from './components/schemes-client';
-
-export default async function SchemesPage() {
-    const schemes = await getSchemes();
-
-    return <SchemesClient schemes={schemes} />;
-}
